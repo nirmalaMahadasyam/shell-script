c41d1c8e84fe39550127686bda387660b999689c
nirmalaMahadasyam <nirmalaMahadasyam@github.com>
2024-04-17 15:02:35 +0530

    my third pull code

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,7 +4,7 @@ PERSON1=$1
 PERSON2=$2 # no space b/w = and value. $ condsider as variable.
 
 
-echo "$PERSON1 :: Hello suresh. How are you?"
+echo "$PERSON1 :: Hello $PERSON2. How are you?"
 echo "$PERSON2 :: I am fine. what about you?"
 echo "$PERSON1 :: I am fine too. how is your work"
 echo "$PERSON2 :: not bad. i am thinking to upgrade devops"
