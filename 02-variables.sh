f38d57cb743e794d66d5b2610acc5d341dec2368
nirmalaMahadasyam <nirmalaMahadasyam@github.com>
2024-04-17 12:37:42 +0530

    my second pull code

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,5 @@
-this is sample file
-echo "welcome to devops"
+#!/bin/bash
+echo "Ramesh :: Hello suresh. How are you?"
+echo "suresh :: I am fine. what about you?"
+echo "Ramesh :: I am fine too. how is your work"
+echo "suresh :: not bad. i am thinking to upgrade devops"
