f67a639246790c148efc8b45d9ef519b5048c2be
nirmalaMahadasyam <nirmalaMahadasyam@github.com>
2024-04-18 23:02:06 +0530

    my practicecode to pull

---
+++ 10-installation.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u) # to run the cmd inside shell script and take the output.i.e, output will holds in USERID
+
+if[$USERID -ne 0]
+then
+echo "Please run this script as root access."
+exit 1 # manually exit if error comes
+
+else
+echo "you are Super User."
+fi
+
+dnf install mysql -y
+
+if[$? -ne 0]
+then
+echo "Installation of mysql....FAILURE"
+exit 1
+else
+echo "Installation of mysql is SUCCESS"
+fi
+
+# 2nd installation code
+
+dnf install git -y
+
+if[$? -ne 0]
+then
+echo "Installation of git....FAILURE"
+exit 1
+else
+echo "Installation of git is SUCCESS"
+fi
+
+
+
+
+
+echo "Is Script Proceding?"
+
