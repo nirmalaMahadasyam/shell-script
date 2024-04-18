a9e04c48336da6348b04e5c2fa9686a31161d59a
nirmalaMahadasyam <nirmalaMahadasyam@github.com>
2024-04-18 15:57:44 +0530

    my practicecode to pull

--- 06-array.sh
+++ 06-array.sh
@@ -5,3 +5,7 @@ MOVIES=("RRR" "DJTillu" "Murari")
 
 echo "All Movies list is :${MOVIES[@]}"
 # To display list of all items
+
+echo "second movoie name is:${MOVIES[1]}"
+echo "Third movoie name is:${MOVIES[2]}"
+# To display selected items 2,3.
