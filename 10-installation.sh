9bea492417bcfb2ce35a640b631cfc9331f85ef4
nirmalaMahadasyam <nirmalaMahadasyam@github.com>
2024-04-18 23:14:32 +0530

    my practicecode to pull

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,20 +4,20 @@ USERID=$(id -u) # to run the cmd inside shell script and take the output.i.e, ou
 
 if[ $USERID -ne 0 ]
 then
-echo "Please run this script as root access."
-exit 1 # manually exit if error comes
+    echo "Please run this script as root access."
+    exit 1 # manually exit if error comes
 else
-echo "you are Super User."
+    echo "you are Super User."
 fi
 
 dnf install mysql -y
 
 if[ $? -ne 0 ]
 then
-echo "Installation of mysql....FAILURE"
-exit 1
+    echo "Installation of mysql....FAILURE"
+    exit 1
 else
-echo "Installation of mysql is SUCCESS"
+     echo "Installation of mysql is SUCCESS"
 fi
 
 # 2nd installation code
@@ -26,15 +26,11 @@ dnf install git -y
 
 if[ $? -ne 0 ]
 then
-echo "Installation of git....FAILURE"
-exit 1
+     echo "Installation of git....FAILURE"
+     exit 1
 else
-echo "Installation of git is SUCCESS"
+     echo "Installation of git is SUCCESS"
 fi
 
-
-
-
-
-echo "Is Script Proceding?"
+#echo "Is Script Proceding?"
 
