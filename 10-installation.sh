45c5cf5e902e7034f20e86cd81171c8ed24b6638
nirmalaMahadasyam <nirmalaMahadasyam@github.com>
2024-04-19 10:33:31 +0530

    installation

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,7 +4,7 @@ USERID=$(id -u) # to run the cmd inside shell script and take the output.i.e, ou
 
 echo "your userid is :: $USERID"
 
-if[ $USERID eq 0]
+if["$USERID"="0"]
 then
 echo "you are super admin"
 exit 1
