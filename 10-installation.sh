c7029e1ee0c063ffcd62f3d9377597e8de8083c5
nirmalaMahadasyam <nirmalaMahadasyam@github.com>
2024-04-19 10:39:17 +0530

    installation

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USERID=$((id -u)) # to run the cmd inside shell script and take the output.i.e, output will holds in USERID
+$USERID=$((id -u)) # to run the cmd inside shell script and take the output.i.e, output will holds in USERID
 
 echo "your userid is :: $USERID"
 
