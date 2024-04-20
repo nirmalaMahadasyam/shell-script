67e41b4e16a69f90e84c226e1079aa968b37d059
nirmalaMahadasyam <nirmalaMahadasyam@github.com>
2024-04-20 05:31:18 +0530

    installation

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%s)
-SCRIPT_NAME=$($0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 #create a function to aviod repeating code
