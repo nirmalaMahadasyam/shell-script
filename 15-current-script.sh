e693da69ed22fd1d811b965b0750774bb6f0050b
nirmalaMahadasyam <nirmalaMahadasyam@github.com>
2024-05-02 10:46:04 +0530

    callingotherscript

---
+++ 15-current-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+COURSE="Devops from Current script"   # variable
+
+echo "Before calling Other scipt: $COURSE"
+echo "PID of the Current script: $$"
+
+# ./16-other-script.sh
+
+
+echo "After calling Other Scipt: $COURSE"
---
+++ 16-other-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+COURSE="Devops from Other Script"   # variable
+
+echo "Variable value from Other Scipt: $COURSE"
+echo "PID of the Other Script: $$"
